8b42e3bf6c3b95504058a0f3e00eb6b5ab1f3952
vaibrainium <vaibhavt459@gmail.com>
2022-08-02 11:25:38 -0700

    build docker image
    initial commit for docker image composition

--- setup.py
+++ setup.py
@@ -2,5 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='src',
+    author='Vaibhav Thakur (vaibrainium)',
     packages=find_packages(),
+    install_requires=[
+    "numpy~=1.23.0",
+    "scipy~=1.8.1",
+    "scikitlearn~=0.24.2",
+    "GPUtil~=1.4.0",
+    "cupy-cuda102~=10.1.0"
+    "seaborn~=0.11.2"
+    ],
 )
