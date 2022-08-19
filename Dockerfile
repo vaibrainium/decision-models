d979c35ddf45e66365442b068d57fb1a7541d3fb
vaibrainium <vaibhavt459@gmail.com>
2022-08-18 21:04:18 -0700

    Setup docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 # Walker lab pytorch image
-FROM cuda-11.6.1-pytorch-1.12.0-torchvision-0.12.0-torchaudio-0.11.0-ubuntu-20.04
+FROM walkerlab/pytorch-jupyter:cuda-11.6.1-pytorch-1.12.0-torchvision-0.12.0-torchaudio-0.11.0-ubuntu-20.04
 LABEL maintainer='vaibrainium (vaibhavt459@gmail.com)'
+
 
 # Copy the code into the container and install it
 COPY . /src/decision_models
-RUN pip install --no-cache /src/decision_models
-
+RUN pip3 --no-cache-dir install -e /src/decision_models
--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
+import sys
+from os import path
+
 from setuptools import find_packages, setup
 
+min_py_version = (3, 7)
+
+if sys.version_info < min_py_version:
+    sys.exit(
+        "Project is only supported for Python {}.{} or higher".format(*min_py_version)
+    )
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, "requirements.txt")) as f:
+    requirements = f.read().split()
+
+
 setup(
-    name='src',
-    author='Vaibhav Thakur (vaibrainium)',
-    packages=find_packages(),
+    name="src",
+    version="0.0.1",
+    description="Model for decision making",
+    long_description="",
+    author="Vaibhav Thakur (vaibrainium)",
+    keywords="database organization",
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     install_requires=[
-    "numpy~=1.23.0",
-    "scipy~=1.8.1",
-    "scikitlearn~=0.24.2",
-    "GPUtil~=1.4.0",
-    "cupy-cuda102~=10.1.0"
-    "seaborn~=0.11.2"
+        requirements,
+        "numpy~=1.23.0",
+        "scipy~=1.8.1",
+        "scikit-learn~=0.24.2",
+        "GPUtil~=1.4.0",
+        "cupy-cuda102~=10.1.0",
+        "seaborn~=0.11.2",
     ],
 )
