19e976449b92f27c6f9e13da686a10b67101b56b
vaibrainium <vaibhavt459@gmail.com>
2025-10-29 02:41:22 +0000

    chore: trying with 3 params vary

--- scripts/slurm/transfer_model_results.sh
+++ scripts/slurm/transfer_model_results.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -r vthakur@klone.hyak.uw.edu:/gscratch/walkerlab/vaibhav/nhp-prior-data/processed/ddm/. /mnt/data/processed/ddm/
+scp -r vthakur@klone.hyak.uw.edu:/gscratch/walkerlab/vaibhav/nhp-prior-data/processed/ddm /mnt/data/processed/
